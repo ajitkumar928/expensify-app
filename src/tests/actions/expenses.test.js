9511155416b0b6688fc175b32ebaf03de54991b2
ajitkumar <ajit9.dutta@gmail.com>
2018-12-28 20:18:22 +0530

    Setup startEditExpenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,6 +1,6 @@
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { startAddExpense, addExpense, editExpense, removeExpense, startRemoveExpense, setExpenses, startSetExpenses } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense, startRemoveExpense, setExpenses, startSetExpenses, startEditExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import database from '../../firebase/firebase';
 import { get } from 'http';
@@ -20,6 +20,24 @@ test('should setup remove expense action object', () => {
   expect(action).toEqual({
     type: 'REMOVE_EXPENSE',
     id: '123abc'
+  });
+});
+
+test('should edit expense from firebase',(done)=>{
+  const store = store.createMockStore({});
+  const id = expenses[0].id;
+  const updates = { amount : 21045 };
+  store.dispatch(startEditExpense(id,updates)).then(()=>{
+      const actions = store.getActions();
+      expect(actions[0]).toEqual({
+          type:'EDIT_EXPENSE',
+          id,
+          updates
+      });
+      return database.ref(`expenses/${id}`).once('value');
+  }).then((snapshot)=>{
+      expect(snapshot.val().amount).toBe(updates.amount);
+      done();
   });
 });
 
